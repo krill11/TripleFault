4b950af25e532d71a3d89f9b3bda667418d0e9e9
ninefox <unvariant.winter@gmail.com>
2023-03-07 20:59:37 -0800

    add vuforia code to Robot class

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Team/OpModes/Autonomous/VuforiaDash.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Team/OpModes/Autonomous/VuforiaDash.kt
@@ -103,7 +103,8 @@ class VuforiaDash : LinearOpMode() {
         }
         targets!!.deactivate()
     }
-    fun identifyTarget(
+
+    private fun identifyTarget(
         targetIndex: Int, targetName: String?, dx: Float, dy: Float, dz: Float, rx: Float, ry: Float,rz: Float
     ) {
         val aTarget = targets!![targetIndex]
@@ -113,8 +114,9 @@ class VuforiaDash : LinearOpMode() {
                 Orientation.getRotationMatrix(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, rx, ry, rz)
             )
     }
+
     companion object {
-        private const val VUFORIA_KEY = "ATuhS5P/////AAABmR8UXvLT90ReroEHDfFojsI7Aa6SANQWVEBufLRaRQVNvLfnvDga0q1F7LQ3sFTKew0TFr6fHjRnvZxKz6g03jljG3Ou26MSob0I59jRyD6NTV6NP0V87CnZ+BMvs8vvAW1lIYJISJBR9wjnhQBk9NnF73J2AXo29TrS7Z+OdmYNsom0JTg6FaIx/aRkhOXVaiVOEs6yC3yRZqLsN2GRJYu3YDxw6O5JYbyIVRnjpRTq5A8HmVKcZsWfkJASiLxpGVP+KWewfd6q/4SgdPvHzrp6yCE4rbfBbhNuxU1xJZDp9rH3CcVPkU7sqmoZezogL0Le60wzRRgQR5GUJ+dj5BCzXIwI1MXPQIgdwdRbTgdm"
+        private val VUFORIA_KEY = System.getProperty("VUFORIA_KEY") ?: throw Exception("VUFORIA_KEY is missing");
         private const val mmPerInch = 25.4f
         private const val mmTargetHeight =
             6 * mmPerInch
