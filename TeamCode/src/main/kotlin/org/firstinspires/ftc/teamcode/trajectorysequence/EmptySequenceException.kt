cae08cbcb202805b50e1a37004056588c1fe7f0c
voxal <aidenhohoho@gmail.com>
2023-03-11 16:46:00 -0800

    reject kotlin, return to java

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/trajectorysequence/EmptySequenceException.kt
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.trajectorysequence
-
-import kotlin.jvm.JvmOverloads
-
-class EmptySequenceException : RuntimeException()
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/trajectorysequence/sequencesegment/WaitSegment.kt
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment
-
-import com.acmerobotics.roadrunner.geometry.Pose2d
-
-class WaitSegment(pose: Pose2d, seconds: Double, markers: List<TrajectoryMarker>) :
-    SequenceSegment(seconds, pose, pose, markers)
